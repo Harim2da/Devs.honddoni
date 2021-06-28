78ee32659209e26e593e3b666f81d85568ede149
윤현기 <yhg920@naver.com>
2021-06-29 02:53:39 +0900

    회원탈퇴 페이지 추가, 테스트용 메인페이지 추가

---
+++ Devs-honddoni/src/com/devs/honddoni/member/view/ChangePwd.java
@@ -0,0 +1,22 @@
+package com.devs.honddoni.member.view;
+
+import javax.swing.JFrame;
+
+import java.awt.Color;
+import java.awt.Rectangle;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class ChangePwd extends JFrame {
+	
+	public ChangePwd() {
+		getContentPane().setForeground(Color.WHITE);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 516, 909);
+		getContentPane().setLayout(null);
+	}
+
+	
+	
+}
--- Devs-honddoni/src/com/devs/honddoni/member/view/MemberView.java
+++ Devs-honddoni/src/com/devs/honddoni/member/view/MemberView.java
@@ -19,14 +19,12 @@ public class MemberView extends JFrame{
 		
 		JPanel panel = new JPanel();
 		panel.setBounds(0, 0, 500, 870);
-		
-		this.setVisible(true);
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		getContentPane().setLayout(null);
 		
 		/* 사이드바 열기 버튼 */
-		JButton btnNewButton = new JButton("");
-		btnNewButton.addActionListener(new ActionListener() {
+		JButton sidebarOpenButton = new JButton("");
+		sidebarOpenButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				
 				/* 사이드바 열기  */
@@ -35,126 +33,127 @@ public class MemberView extends JFrame{
 		});
 		
 		/* 매칭 알림 표시 */
-		JLabel lblNewLabel_2 = new JLabel("");
-		lblNewLabel_2.setIcon(new ImageIcon(MemberView.class.getResource("/member/myPage/matchingSign.png")));
-		lblNewLabel_2.setBounds(254, 583, 41, 41);
-		getContentPane().add(lblNewLabel_2);
+		JLabel matchingSign = new JLabel("");
+		matchingSign.setIcon(new ImageIcon(MemberView.class.getResource("/member/myPage/matchingSign.png")));
+		matchingSign.setBounds(254, 583, 41, 41);
+		getContentPane().add(matchingSign);
 		
 		/* 혼또니 로고 버튼 */
-		JButton btnNewButton_3 = new JButton("");
-		btnNewButton_3.addActionListener(new ActionListener() {
+		JButton honddoniLogo = new JButton("");
+		honddoniLogo.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				
 				/* 메인메뉴 화면으로 이동하기 */
 				
 			}
 		});
-		btnNewButton_3.setContentAreaFilled(false);
-		btnNewButton_3.setBorderPainted(false);
-		btnNewButton_3.setIcon(new ImageIcon(MemberView.class.getResource("/common/honddoniLogo.png")));
-		btnNewButton_3.setBounds(304, 27, 173, 71);
-		getContentPane().add(btnNewButton_3);
-		btnNewButton.setContentAreaFilled(false);
-		btnNewButton.setBorderPainted(false);
-		btnNewButton.setIcon(new ImageIcon(MemberView.class.getResource("/common/Group 1014.png")));
-		btnNewButton.setBounds(35, 33, 34, 20);
-		getContentPane().add(btnNewButton);
+		honddoniLogo.setContentAreaFilled(false);
+		honddoniLogo.setBorderPainted(false);
+		honddoniLogo.setIcon(new ImageIcon(MemberView.class.getResource("/common/honddoniLogo.png")));
+		honddoniLogo.setBounds(304, 27, 173, 71);
+		getContentPane().add(honddoniLogo);
+		sidebarOpenButton.setContentAreaFilled(false);
+		sidebarOpenButton.setBorderPainted(false);
+		sidebarOpenButton.setIcon(new ImageIcon(MemberView.class.getResource("/common/Group 1014.png")));
+		sidebarOpenButton.setBounds(35, 33, 34, 20);
+		getContentPane().add(sidebarOpenButton);
 		
 		/* 비밀번호 변경 버튼 */
-		JButton btnNewButton_1 = new JButton("");
-		btnNewButton_1.addActionListener(new ActionListener() {
+		JButton changePwdButton = new JButton("");
+		changePwdButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				
 				/* 비밀번호 변경 버튼 클릭 후 */
+//				new ChangePwd();
 				
 			}
 		});
-		btnNewButton_1.setIcon(new ImageIcon(MemberView.class.getResource("/member/myPage/pwdchange.png")));
-		btnNewButton_1.setContentAreaFilled(false);
-		btnNewButton_1.setBorderPainted(false);
-		btnNewButton_1.setBounds(92, 153, 137, 137);
-		getContentPane().add(btnNewButton_1);
+		changePwdButton.setIcon(new ImageIcon(MemberView.class.getResource("/member/myPage/pwdchange.png")));
+		changePwdButton.setContentAreaFilled(false);
+		changePwdButton.setBorderPainted(false);
+		changePwdButton.setBounds(92, 153, 137, 137);
+		getContentPane().add(changePwdButton);
 		
 		/* 개인정보 변경 버튼 */
-		JButton btnNewButton_2 = new JButton("");
-		btnNewButton_2.addActionListener(new ActionListener() {
+		JButton changeMemberinfo = new JButton("");
+		changeMemberinfo.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				
 				/* 개인정보 변경 클릭 후 */
 				
 			}
 		});
-		btnNewButton_2.setIcon(new ImageIcon(MemberView.class.getResource("/member/myPage/changeInfo.png")));
-		btnNewButton_2.setContentAreaFilled(false);
-		btnNewButton_2.setBorderPainted(false);
-		btnNewButton_2.setBounds(272, 153, 137, 137);
-		getContentPane().add(btnNewButton_2);
+		changeMemberinfo.setIcon(new ImageIcon(MemberView.class.getResource("/member/myPage/changeInfo.png")));
+		changeMemberinfo.setContentAreaFilled(false);
+		changeMemberinfo.setBorderPainted(false);
+		changeMemberinfo.setBounds(272, 153, 137, 137);
+		getContentPane().add(changeMemberinfo);
 		
 		/* 탈퇴하기 버튼 */
-		JButton btnNewButton_1_1 = new JButton("");
-		btnNewButton_1_1.addActionListener(new ActionListener() {
+		JButton resignMemberButton = new JButton("");
+		resignMemberButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				
 				/* 탈퇴하기 버튼 클릭 후 */
 				
 			}
 		});
-		btnNewButton_1_1.setIcon(new ImageIcon(MemberView.class.getResource("/member/myPage/resign.png")));
-		btnNewButton_1_1.setContentAreaFilled(false);
-		btnNewButton_1_1.setBorderPainted(false);
-		btnNewButton_1_1.setBounds(92, 380, 137, 137);
-		getContentPane().add(btnNewButton_1_1);
+		resignMemberButton.setIcon(new ImageIcon(MemberView.class.getResource("/member/myPage/resign.png")));
+		resignMemberButton.setContentAreaFilled(false);
+		resignMemberButton.setBorderPainted(false);
+		resignMemberButton.setBounds(92, 380, 137, 137);
+		getContentPane().add(resignMemberButton);
 		
 		/* 관심글 모아보기 버튼 */
-		JButton btnNewButton_1_2 = new JButton("");
-		btnNewButton_1_2.addActionListener(new ActionListener() {
+		JButton InterestingPostButton = new JButton("");
+		InterestingPostButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				
 				/* 관심글 모아보기 클릭 후 */
 				
 			}
 		});
-		btnNewButton_1_2.setIcon(new ImageIcon(MemberView.class.getResource("/member/myPage/heart.png")));
-		btnNewButton_1_2.setContentAreaFilled(false);
-		btnNewButton_1_2.setBorderPainted(false);
-		btnNewButton_1_2.setBounds(272, 380, 137, 137);
-		getContentPane().add(btnNewButton_1_2);
+		InterestingPostButton.setIcon(new ImageIcon(MemberView.class.getResource("/member/myPage/heart.png")));
+		InterestingPostButton.setContentAreaFilled(false);
+		InterestingPostButton.setBorderPainted(false);
+		InterestingPostButton.setBounds(272, 380, 137, 137);
+		getContentPane().add(InterestingPostButton);
 		
 		/* 업적 & 포인트 버튼 */
-		JButton btnNewButton_1_3 = new JButton("");
-		btnNewButton_1_3.addActionListener(new ActionListener() {
+		JButton rewardButton = new JButton("");
+		rewardButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				
 				/* 업적 및 포인트 버튼 클릭 후 */
 				
 			}
 		});
-		btnNewButton_1_3.setIcon(new ImageIcon(MemberView.class.getResource("/member/myPage/medal.png")));
-		btnNewButton_1_3.setContentAreaFilled(false);
-		btnNewButton_1_3.setBorderPainted(false);
-		btnNewButton_1_3.setBounds(92, 601, 137, 137);
-		getContentPane().add(btnNewButton_1_3);
+		rewardButton.setIcon(new ImageIcon(MemberView.class.getResource("/member/myPage/medal.png")));
+		rewardButton.setContentAreaFilled(false);
+		rewardButton.setBorderPainted(false);
+		rewardButton.setBounds(92, 601, 137, 137);
+		getContentPane().add(rewardButton);
 		
 		/* 내글 모아보기 버튼 */
-		JButton btnNewButton_1_4 = new JButton("");
-		btnNewButton_1_4.addActionListener(new ActionListener() {
+		JButton writtenPostButton = new JButton("");
+		writtenPostButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				
 				/* 내글 모아보기 클릭 후 */
 				
 			}
 		});
-		btnNewButton_1_4.setIcon(new ImageIcon(MemberView.class.getResource("/member/myPage/moabogi.png")));
-		btnNewButton_1_4.setContentAreaFilled(false);
-		btnNewButton_1_4.setBorderPainted(false);
-		btnNewButton_1_4.setBounds(272, 601, 137, 137);
-		getContentPane().add(btnNewButton_1_4);
+		writtenPostButton.setIcon(new ImageIcon(MemberView.class.getResource("/member/myPage/moabogi.png")));
+		writtenPostButton.setContentAreaFilled(false);
+		writtenPostButton.setBorderPainted(false);
+		writtenPostButton.setBounds(272, 601, 137, 137);
+		getContentPane().add(writtenPostButton);
 		
-		/* */
-		JLabel lblNewLabel_1 = new JLabel("");
-		lblNewLabel_1.setIcon(new ImageIcon(MemberView.class.getResource("/member/myPage/mypageName2.png")));
-		lblNewLabel_1.setBounds(0, 0, 500, 870);
-		getContentPane().add(lblNewLabel_1);
+		/* 마이페이지에 있는 버튼 밑에 글씨들 */
+		JLabel menuNameLabel = new JLabel("");
+		menuNameLabel.setIcon(new ImageIcon(MemberView.class.getResource("/member/myPage/mypageName2.png")));
+		menuNameLabel.setBounds(0, 0, 500, 870);
+		getContentPane().add(menuNameLabel);
 		
 		
 	}
