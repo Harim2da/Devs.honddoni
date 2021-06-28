2e5b8673b2ff61ead017202ded4e2f42da25e65f
Harim <helloharim09@gmail.com>
2021-06-28 23:09:45 +0900

    post 뷰 작업 시작

--- Devs-honddoni/src/com/devs/honddoni/post/view/PostView.java
+++ Devs-honddoni/src/com/devs/honddoni/post/view/PostView.java
@@ -1,5 +1,53 @@
 package com.devs.honddoni.post.view;
 
-public class PostView {
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+
+public class PostView extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					PostView frame = new PostView();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public PostView() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 516, 909);
+		
+		contentPane = new JPanel();
+		//contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JPanel panel = new JPanel();
+		panel.setBounds(0, 0, 500, 870);
+		contentPane.add(panel);
+		panel.setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("New label");
+		lblNewLabel.setBounds(0, 0, 500, 870);
+		panel.add(lblNewLabel);
+	}
 
 }
