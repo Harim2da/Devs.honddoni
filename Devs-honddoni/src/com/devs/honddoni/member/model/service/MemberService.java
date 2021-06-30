73ff9240799b1a958fd3bdd57f90bbc1622bd81b
yeseul JO <cysysc102@gmail.com>
2021-06-30 23:28:49 +0900

    registMember, 컨트롤러, 서비스 작업중...

--- Devs-honddoni/src/com/devs/honddoni/member/model/service/MemberService.java
+++ Devs-honddoni/src/com/devs/honddoni/member/model/service/MemberService.java
@@ -1,5 +1,24 @@
 package com.devs.honddoni.member.model.service;
 
+import java.sql.Connection;
+
+import com.devs.honddoni.member.model.dao.MemberDAO;
+
 public class MemberService {
+	
+	private MemberDAO memberDAO;
+	
+	public MemberService() {
+		this.memberDAO = new MemberDAO();
+	}
+
+	public int idDuplCheck(String getUserId) {
+		
+		Connection con = getConnection();
+		
+		int result = 1;
+		
+		return result;
+	}
 
 }
