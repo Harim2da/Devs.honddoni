4658da9c15aa6fb0184e6e38ca4a03f15440b35b
Harim <helloharim09@gmail.com>
2021-07-02 03:56:32 +0900

    DB등록 직전

--- Devs-honddoni/src/com/devs/honddoni/post/model/dao/PostDAO.java
+++ Devs-honddoni/src/com/devs/honddoni/post/model/dao/PostDAO.java
@@ -1,5 +1,19 @@
 package com.devs.honddoni.post.model.dao;
 
+import java.sql.Connection;
+
+import com.devs.honddoni.common.dto.PostDTO;
+
 public class PostDAO {
+
+	public int insertNewHonPost(Connection con, PostDTO post) {
+		//게시글 번호 - DB에서 받, 카테고리명 /지역명은 DTO에서 String으로 받아서 여기서 int로 변환 
+		
+		
+		
+		
+		
+		return 0;
+	}
 
 }
