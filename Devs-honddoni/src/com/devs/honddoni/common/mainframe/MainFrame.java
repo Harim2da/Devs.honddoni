57edb80cad1169bbc7c1a83a8d59451919abcdd9
Harim <helloharim09@gmail.com>
2021-07-01 22:06:56 +0900

    메인프레임 수정 재업

---
+++ Devs-honddoni/src/com/devs/honddoni/common/mainframe/MainFrame.java
@@ -0,0 +1,34 @@
+package com.devs.honddoni.common.mainframe;
+
+import java.awt.Color;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class MainFrame extends JFrame{
+	
+	private JPanel topPanel;
+	
+	public MainFrame() {
+		this.setBounds(100, 100, 516, 909);
+		this.setLayout(null);
+		
+		this.add(topPanel);
+		
+		this.setVisible(true);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+	
+	
+	public void topPanel() {
+		topPanel = new JPanel();
+		topPanel.setBounds(0, 0, 500, 100);
+		topPanel.setLayout(null);
+		topPanel.setBackground(Color.WHITE);
+
+	}
+	
+	
+	
+	
+}
