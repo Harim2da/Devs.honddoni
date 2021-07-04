68d38a1d5d94b6b4d62301b876c93f290aa21643
정현수 <hs36799@gmail.com>
2021-07-04 22:01:12 +0900

    정현수 수정

--- Devs-honddoni/src/com/devs/honddoni/post/view/Ctest2.java
+++ Devs-honddoni/src/com/devs/honddoni/post/view/Ctest2.java
@@ -4,19 +4,25 @@ import javax.swing.JFrame;
 
 public class Ctest2 {
 
+	static JFrame jf = new JFrame();
+	
 	public static void main(String[] args) {
 		
-		JFrame jf = new JFrame();
+//		JFrame jf = new JFrame();
 
 		
+	
 		jf.setBounds(100, 100, 516, 909);
 		jf.setLayout(null);
 		
 		SelectedComment cm = new SelectedComment();
+//		SelectedComment2 cm2 = new SelectedComment2();
 		cm.collect();
 		
 		jf.add(cm.getUpPanel());
 		jf.add(cm.getDownPanel());
+//		jf.add(cm.getNextPanel());
+//		jf.add(cm2.getDownPanel2());
 		
 		
 		jf.setVisible(true);
