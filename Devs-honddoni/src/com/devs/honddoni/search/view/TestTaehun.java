05828a1d7241e55706fe8ea21bf80f0c2b52a60d
Taehun Kim <tehonkim.kim@gamil.com>
2021-07-06 02:28:16 +0900

    중간정리

--- Devs-honddoni/src/com/devs/honddoni/search/view/TestTaehun.java
+++ Devs-honddoni/src/com/devs/honddoni/search/view/TestTaehun.java
@@ -1,27 +1,24 @@
 package com.devs.honddoni.search.view;
 
-import com.devs.honddoni.search.controller.SearchController;
-
 public class TestTaehun {
 
 	public static void main(String[] args) {
 		
 	
-//		new MainBottomPanel(); // 실행
+//		new MainBottomPanel(); // 실행, (실행시켜서 전체조회 or 자유게시판 누르면 위에 패널 하나 더 생기는데 나오지 않음)
 		
-		new AllBoardList(); // 실행X, controller의 Application에서 DB내용은 조회 가능
+//		new AllBoardList(); // 실행X, controller의 Application에서 DB내용은 조회 가능
 		
 //		new FreeBoard(); // 실행X, (왜 현기님꺼 패널이 뜨지...?? <- 메인프레임 받아서 그런거임), controller의 Application에서 DB내용은 조회 가능  
 		
-//		new PostFreeBoard(); 실행, 글씨 써짐(게시물 등록 버튼 -> 게시물 등록 아직 안됌)
+//		new PostFreeBoard(); // 실행, 글씨 써짐(게시물 등록 버튼 -> 게시물 등록 아직 안됌)
 
-		
+		new FreeBoardFrame();
 		
 		
 //		new SearchBoard();
 //		new SearchBoardList();
-//		
-//		new AllBoardListView();
+//		new AllBoardListView();	
 		
 //		SearchController sc = new SearchController();
 //		sc.selectSearch();
