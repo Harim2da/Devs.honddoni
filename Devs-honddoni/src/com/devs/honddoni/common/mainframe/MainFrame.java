adaf44c6211b7b5ff87088ec5bd8b28a5102cfc7
윤현기 <yhg920@naver.com>
2021-07-07 02:51:18 +0900

    1231231

--- Devs-honddoni/src/com/devs/honddoni/common/mainframe/MainFrame.java
+++ Devs-honddoni/src/com/devs/honddoni/common/mainframe/MainFrame.java
@@ -6,25 +6,10 @@ import java.io.IOException;
 import javax.imageio.ImageIO;
 import javax.swing.JFrame;
 
-
+import com.devs.honddoni.admin.viewpenel.Notice;
+import com.devs.honddoni.member.view.MyPage;
 import com.devs.honddoni.memberLog.view.FirstView;
 import com.devs.honddoni.post.view.SelectAllHonPost;
-
-
-import com.devs.honddoni.admin.viewpenel.AdminList;
-import com.devs.honddoni.admin.viewpenel.AdminList1;
-import com.devs.honddoni.admin.viewpenel.AdminList2;
-
-import com.devs.honddoni.memberLog.view.FirstView;
-import com.devs.honddoni.search.view.AllBoardList;
-
-import com.devs.honddoni.admin.viewpenel.AdminList;
-import com.devs.honddoni.admin.viewpenel.Notice;
-import com.devs.honddoni.memberLog.view.FirstView;
-import com.devs.honddoni.post.view.PostHonddoni;
-import com.devs.honddoni.post.view.PostModify;
-import com.devs.honddoni.search.view.MainBottomPanel;
-import com.devs.honddoni.search.view.PostFreeBoard;
 
 
 
@@ -47,16 +32,16 @@ public class MainFrame extends JFrame{
 			e.printStackTrace();
 		}
 
-//		new MyPage(this);
+		new MyPage(this);
 
-		new FirstView(this);
+//		new FirstView(this);
 
 //		new PostFreeBoard(this);
 
 		
 //		new SelectedComment(this, 1);
 
-		new SelectAllHonPost(this);
+//		new SelectAllHonPost(this);
 
 
 
@@ -72,7 +57,7 @@ public class MainFrame extends JFrame{
 		
 		//예슬 실험중
 //		new FirstView(this);
-		new Notice(this);
+//		new Notice(this);
 		
 		//태훈 실험중(메인바텀패널)
 //		new MainBottomPanel(this);
